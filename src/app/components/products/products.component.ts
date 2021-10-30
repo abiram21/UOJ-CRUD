ccd873138f6a0668b436a75159bf65af8423d225
Abiram <ram.abiram21@gmail.com>
2021-10-30 16:53:13 +0530

    Inital commit created home and product

---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
